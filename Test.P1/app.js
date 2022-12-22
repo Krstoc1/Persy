7dc6ea2edb9bde3471bc79428fafa2fe5fd7990e
Krstoc1 <94562460+Krstoc1@users.noreply.github.com>
2022-12-21 21:34:25 -0600

    test1

---
+++ Test.P1/app.js
@@ -0,0 +1,46 @@
+// app.js
+// Set up the scene, camera, and renderer
+const scene = new THREE.Scene();
+
+// Set up a random background color
+
+const backgroundColor = new THREE.Color(Math.random(), Math.random(), Math.random());
+scene.background = backgroundColor;
+
+
+
+const camera = new THREE.PerspectiveCamera(
+  75, // field of view
+  window.innerWidth / window.innerHeight, // aspect ratio
+  0.1, // near plane
+  1000 // far plane
+);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById("canvas-container").appendChild(renderer.domElement);
+
+// Set up OrbitControls
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enableZoom = true;
+
+// Create a texture
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load("aset/Cat03.jpg");
+
+// Create a sphere and add it to the scene
+const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+const sphereMaterial = new THREE.MeshBasicMaterial({ map: texture });
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphere);
+
+// Render the scene
+function render() {
+  requestAnimationFrame(render);
+  controls.update();
+  renderer.render(scene, camera);
+}
+render();
